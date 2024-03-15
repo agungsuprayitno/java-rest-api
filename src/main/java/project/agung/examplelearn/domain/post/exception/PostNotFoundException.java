4aabf722f5fbcc2018547644b05c9c7f13f69ee0
Agung Suprayitno <agung.suprayitno@kawanlamacorp.com>
2024-03-15 14:47:24 +0700

    Remove unecessary annotation.

--- src/main/java/project/agung/examplelearn/domain/post/exception/PostNotFoundException.java
+++ src/main/java/project/agung/examplelearn/domain/post/exception/PostNotFoundException.java
@@ -1,8 +1,5 @@
 package project.agung.examplelearn.domain.post.exception;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
 public class PostNotFoundException extends Exception {
     public PostNotFoundException() { super("post-not-found"); }
 }
